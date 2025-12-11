d95342e29c3e9f1776156bdcb5e30e3d601bfd42
amateurorchestraunion-web <amateurorchestraunion@gmail.com>
2025-12-11 18:26:24 +0900

    Create firebase.js

---
+++ assets/js/firebase.js
@@ -0,0 +1,23 @@
+// firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
+import { 
+  getFirestore, collection, doc, setDoc, getDocs, onSnapshot 
+} from "https://www.gstatic.com/firebasejs/12.6.0/firebase-firestore.js";
+
+// Firebase 설정
+const firebaseConfig = {
+  apiKey: "AIzaSyDtat8CbbHg4lnlZ3cQzA2XjX872vzV9iE",
+  authDomain: "aou-event.firebaseapp.com",
+  projectId: "aou-event",
+  storageBucket: "aou-event.firebasestorage.app",
+  messagingSenderId: "650092885024",
+  appId: "1:650092885024:web:bb8e546e8a0b9240a3b845",
+  measurementId: "G-700FX71W9F"
+};
+
+// Firebase 초기화
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+// tree.js에서 사용 가능하도록 export
+export { db, collection, doc, setDoc, getDocs, onSnapshot };
