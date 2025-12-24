2fdbb6fd78f85d2e017e6366d965edb033ae5e67
amateurorchestraunion-web <amateurorchestraunion@gmail.com>
2025-12-24 17:42:46 +0900

    Update tree.js

--- assets/js/tree.js
+++ assets/js/tree.js
@@ -26,7 +26,7 @@ const deleteBtn = document.querySelector(".delete-btn");
 
 let currentOrnamentId = null;
 
-const BASE_ORNAMENT_SIZE = 60;
+const BASE_ORNAMENT_SIZE = 120;
 const isMobile = window.matchMedia("(max-width: 768px)").matches;
 
 /* ==============================================
