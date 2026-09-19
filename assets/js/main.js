a97f4bbb73bac67d0d4d61806394a868f1d9abb4
amateurorchestraunion-web <amateurorchestraunion@gmail.com>
2026-09-19 22:48:24 +0900

    d

--- assets/js/main.js
+++ assets/js/main.js
@@ -146,7 +146,7 @@
       initIsotope = new Isotope(isotopeItem.querySelector('.isotope-container'), {
         itemSelector: '.isotope-item',
         layoutMode: layout,
-        filter: '.filter-2015',
+        filter: '.filter-2025',
         sortBy: sort
       });
     });
